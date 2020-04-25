d27a1c9eb9514c8102009ef6053fd7faf0d8b395
Gustė Žilinskaitė <51269699+Metsuge@users.noreply.github.com>
2020-04-25 16:05:44 +0300

    Add files via upload

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -5,23 +5,17 @@ const Featured = () => {
     return(<>
 
     <div className='main-featured'>
-        <div className='top-text'>
-            <li><button id='button-new-article'>NEW ARTICLE</button></li>
-            <li className='word-published'>Published Yesterday</li>
-            <p className='word-featured'>FEATURED</p>
-        </div>
-        
+        <li><button id='button-new-article'>NEW ARTICLE</button></li>
+        <li className='word-published'>Published Yesterday</li>
+        <p className='word-featured'>FEATURED</p>
         <p className='text'>
-        <h2>Exploring the Curonian Spit</h2>
-            <p className='long-text'>
-                The Curonian Spit is a 98 km long, thin, curved sand-dune spit that separates the Curonian Lagoon from the Baltic
-                Sea coast. Its southern portion lies within Kaliningrad Oblast, Russia and its northern within southwestern Lithuania. It is a UNESCO World Heritage Site shared by the two countries.
-            </p>
+            <h2>Exploring the Curonian Spit</h2>
+                <p className='long-text'>
+                    The Curonian Spit is a 98 km long, thin, curved sand-dune spit that separates the Curonian Lagoon from the Baltic
+                    Sea coast. Its southern portion lies within Kaliningrad Oblast, Russia and its northern within southwestern Lithuania. It is a UNESCO World Heritage Site shared by the two countries.
+                </p>
         </p>
         <button className='button-article'>Read the article</button>
-               
-           
-    
     </div>
         </>
 
