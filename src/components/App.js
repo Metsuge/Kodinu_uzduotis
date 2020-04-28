2689f4b33d8ff281f3ef5d9166eb924a05690b68
Gustė Žilinskaitė <51269699+Metsuge@users.noreply.github.com>
2020-04-28 17:18:52 +0300

    Add files via upload

--- src/components/App.js
+++ src/components/App.js
@@ -52,25 +52,28 @@ export class App extends Component {
                             </li>
                         
                         <br></br>
-                        <li>Our portfolio</li>
-                        <li>daumantasbanys.lt</li>
+                        <li><a href='#'>Our portfolio</a></li>
+                        <li><a>daumantasbanys.lt</a></li>
                     </ul>
                     <ul>
-                        <li>Links</li>
-                        <li>About us</li>
-                        <li>Contact us</li>
-                        <li>Sitemap</li>
-                        <li>Contribute</li>
+                        <li><a href='#'>Links</a></li>
+                        <li><a href='#'>About us</a></li>
+                        <li><a href='#'>Contact us</a></li>
+                        <li><a href='#'>Sitemap</a></li>
+                        <li><a href='#'>Contribute</a></li>
                     </ul>
                     <ul >
-                        <li>Navigation</li>
-                        <li>Home</li>
-                        <li>Discover</li>
-                        <li>Search</li>
-                        <li>Upload</li>
+                        <li><a href='#'>Navigation</a></li>
+                        <li><a href='#'>Home</a></li>
+                        <li><a href='#'>Discover</a></li>
+                        <li><a href='#'>Search</a></li>
+                        <li><a href='#'>Upload</a></li>
                     </ul>
                     <ul id='footer-left'>
-                        <li>EXPLORD <span className='links'>&copy; 2018</span></li><br/>
+                        <li id='logo'>
+                            <span style={{ fontSize:'20px', color:'#404040'}} ><span style={{fontWeight:'bold'}}>EXP</span>LORD</span>
+                            <span className='copy'>&copy; 2018</span>
+                        </li><br/>
                         <li>A Travelers' Blog</li>
                         <li className='footer-text'>Our main focus is to provide high quality, objective,<br/>
                         and informative travelling content from over the world.</li>
